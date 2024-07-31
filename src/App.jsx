e1f77970759fc75de1b81ca6b6c6748d2a39527e
Nana5aki <lvxuanrui1@outlook.com>
2024-08-01 00:04:22 +0800

    2024/08/01

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 /*
  * @Author: lvxr
- * @LastEditTime: 2024-07-31 23:04:33
+ * @LastEditTime: 2024-07-31 23:50:47
  */
+
+import Nav from "./components/Nav"
 
 function App() {
   return (
     <div>
+      <Nav/>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
 /*
  * @Author: lvxr
- * @LastEditTime: 2024-07-31 23:07:13
+ * @LastEditTime: 2024-07-31 23:24:59
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
