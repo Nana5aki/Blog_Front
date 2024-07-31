7a6c508a40ec339def80796d3a26d59d7d482010
Nana5aki <lvxuanrui1@outlook.com>
2024-07-31 23:21:33 +0800

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: lvxr
+ * @LastEditTime: 2024-07-31 23:04:33
+ */
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: lvxr
+ * @LastEditTime: 2024-07-31 23:07:13
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
